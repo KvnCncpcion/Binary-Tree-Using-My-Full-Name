a187f42fabbc19130d6628ec81890a79b6de5a31
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-14 20:00:22 +0800

    Defining build_tree function

--- Full Name in Order.py
+++ Full Name in Order.py
@@ -30,3 +30,12 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal()
 
         return elements
+
+def build_tree(elements):
+    print("This is my full name:",elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
