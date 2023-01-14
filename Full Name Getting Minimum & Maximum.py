caa303959b25d2df091c5de69437b0bf80977746
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-14 22:50:27 +0800

    Defining add_child, search, in_order_traversal, pre_order_traversal, post_order_traversal, delete, get_minimum and get_maximum functions

--- Full Name Getting Minimum & Maximum.py
+++ Full Name Getting Minimum & Maximum.py
@@ -3,3 +3,100 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def pre_order_traversal(self):
+        elements = []
+        elements.append(self.data)
+        if self.left:
+            elements += self.left.pre_order_traversal()
+
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.post_order_traversal()
+
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            minimum_val = self.right.get_minimum()
+            self.data = minimum_val
+            self.right = self.right.delete(minimum_val)
+
+        return self
+
+    def get_minimum(self):
+        if self.left is None:
+            return self.data
+        return self.left.get_minimum()
+
+    def get_maximum(self):
+        if self.right is None:
+            return self.data
+        return self.right.get_maximum()
